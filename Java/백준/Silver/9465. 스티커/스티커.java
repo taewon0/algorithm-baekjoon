f2a1ee62f8af5a0e105f0e5deddffa6ab39f9cec
taewon kim <128652249+taewon0@users.noreply.github.com>
2025-12-14 18:55:54 +0900

    [Silver I] Title: 스티커, Time: 704 ms, Memory: 120160 KB -BaekjoonHub

--- Java/백준/Silver/9465. 스티커/스티커.java
+++ Java/백준/Silver/9465. 스티커/스티커.java
@@ -13,36 +13,21 @@ public class Main {
 		while (t-->0) {
 			int n = Integer.parseInt(br.readLine());
 			int[][] sticker = new int[2][n];
-			int[][][] d = new int[2][n][2];
+			int[][] d = new int[3][n];
 			st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < n; i++) sticker[0][i] = Integer.parseInt(st.nextToken());
 			st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < n; i++) sticker[1][i] = Integer.parseInt(st.nextToken());
 			
-			d[0][0][0] = sticker[1][0];
-			d[0][0][1] = sticker[0][0];
-			d[1][0][0] = sticker[0][0];
-			d[1][0][1] = sticker[1][0];
+			d[0][0] = sticker[0][0];
+			d[1][0] = sticker[1][0];
+			d[2][0] = 0;
 			for (int i = 1; i < n; i++) {
-				for (int j = 0; j < 2; j++) {
-					if (j == 0) {
-						d[j][i][0] = Math.max(d[j][i-1][0], d[j][i-1][1]);
-						d[j][i][1] = d[j][i-1][0] + sticker[j][i];
-					}
-					else if (j == 1) {
-						d[j][i][0] = Math.max(d[j][i-1][0], d[j][i-1][1]);
-						d[j][i][0] = Math.max(d[j][i][0], d[0][i][0]);
-						d[j][i][0] = Math.max(d[j][i][0], d[0][i][1]);
-						
-						d[j][i][1] = d[j][i-1][0] + sticker[j][i];
-						
-						d[0][i][0] = Math.max(d[0][i][0], d[j][i][1]);
-					}
-//					System.out.print(d[j][i][0]);
-//					System.out.println(" "+d[j][i][1]);
-				}
+				d[0][i] = Math.max(d[1][i-1], d[2][i-1]) + sticker[0][i];
+				d[1][i] = Math.max(d[0][i-1], d[2][i-1]) + sticker[1][i];
+				d[2][i] = Math.max(Math.max(d[0][i-1], d[1][i-1]), d[2][i-1]);
 			}
-			sb.append(Math.max(d[1][n-1][0], d[1][n-1][1])).append("\n");
+			sb.append(Math.max(Math.max(d[0][n-1], d[1][n-1]), d[2][n-1])).append("\n");
 		}
 		System.out.println(sb);
 	}
