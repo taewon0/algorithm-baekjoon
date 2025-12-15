3e648baa5a1819f0787d7bb5f6d23a170b5e5462
taewon kim <128652249+taewon0@users.noreply.github.com>
2025-12-15 16:11:50 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1568 ms, Memory: 336384 KB -BaekjoonHub

--- Java/백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ Java/백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,22 +1,25 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[n];
-		int i = n;
-		while (i-->0) {
-			arr[i] = Integer.parseInt(br.readLine());
+		int[] arr = new int[10001];
+		for (int i = 0; i < n; i++) {
+			int num = Integer.parseInt(br.readLine());
+			arr[num]++;
 		}
-		Arrays.sort(arr);
 		StringBuilder sb = new StringBuilder();
-		i = -1;
-		while (++i<n) {
-			sb.append(arr[i]).append("\n");
+		for (int i = 1; i < 10001; i++) {
+			for (int j = 0; j < arr[i]; j++) {
+				sb.append(i).append("\n");
+			}
 		}
 		System.out.println(sb);
 	}
