c30b16fceb1f7a13b662e063c631c461c38c2978
taewon kim <128652249+taewon0@users.noreply.github.com>
2025-12-17 20:48:20 +0900

    [Gold IV] Title: 거짓말, Time: 108 ms, Memory: 14288 KB -BaekjoonHub

--- Java/백준/Gold/1043. 거짓말/거짓말.java
+++ Java/백준/Gold/1043. 거짓말/거짓말.java
@@ -1,59 +1,67 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
+	
+	static int[] parent;
+	
+	static int find(int x) {
+		if (parent[x] == x) return x;
+		return parent[x] = find(parent[x]);
+	}
+	
+	static void union(int a, int b) {
+		a = find(a);
+		b = find(b);
+		if (a != b) parent[b] = a;
+	}
 
-	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
+		
+		parent = new int[n + 1];
+		for (int i = 1; i <= n; i++) parent[i] = i;
+		
 		st = new StringTokenizer(br.readLine());
 		int tn = Integer.parseInt(st.nextToken());
-		HashSet<Integer> t = new HashSet<>();
+		int[] t = new int[tn];
 		for (int i = 0; i < tn; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			t.add(num);
+			t[i] = Integer.parseInt(st.nextToken());
 		}
-		HashSet<Integer>[] party = new HashSet[m];
-		for (int i = 0; i < m; i++) {
-			party[i] = new HashSet<>();
+		//진실 아는 사람들 union
+		for (int i = 1; i < tn; i++) {
+			union(t[0], t[i]);
 		}
+		ArrayList<int[]> party = new ArrayList<>();
 		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
 			int pn = Integer.parseInt(st.nextToken());
+			int[] arr = new int[pn];
 			for (int j = 0; j < pn; j++) {
-				int num = Integer.parseInt(st.nextToken());
-				party[i].add(num);
+				arr[j] = Integer.parseInt(st.nextToken());
+			}
+			party.add(arr);
+			//파티원들 union
+			for (int j = 1; j < pn; j++) {
+				union(arr[0], arr[j]);
 			}
 		}
-		boolean changed = true;
-		while (changed) {
-			changed = false;
-			for (int i = 0; i < m; i++) {
-				boolean isKnow = false;
-				for (int num : party[i]) {
-					if (t.contains(num)) {
-						isKnow = true;
-						break;
-					}
-				}
-				if (isKnow) {
-					int oriSize = t.size();
-					t.addAll(party[i]);
-					if (oriSize < t.size()) changed = true;
-				}
-			}
+		if (tn ==  0) {
+			System.out.println(m);
+			return;
 		}
 		int answer = 0;
-		for (int i = 0; i < m; i++) {
+		int troot = find(t[0]);
+		for (int[] par : party) {
 			boolean isKnow = false;
-			for (int num : party[i]) {
-				if (t.contains(num)) {
+			for (int p : par) {
+				if (find(p) == troot) {
 					isKnow = true;
 					break;
 				}
