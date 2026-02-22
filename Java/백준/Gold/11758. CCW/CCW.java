994d1e3fbe9f305df389a41900b514736634ac45
taewon kim <128652249+taewon0@users.noreply.github.com>
2026-02-22 18:33:06 +0900

    [Gold V] Title: CCW, Time: 104 ms, Memory: 14376 KB -BaekjoonHub

---
+++ Java/백준/Gold/11758. CCW/CCW.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int x1 = Integer.parseInt(st.nextToken());
+		int y1 = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int x2 = Integer.parseInt(st.nextToken());
+		int y2 = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int x3 = Integer.parseInt(st.nextToken());
+		int y3 = Integer.parseInt(st.nextToken());
+		int a = x1 * y2 + x2 * y3 + x3 * y1;
+		int b = y1 * x2 + y2 * x3 + y3 * x1;
+		if (a - b == 0) System.out.println(0);
+		else if (a - b > 0) System.out.println(1);
+		else System.out.println(-1);
+	}
+
+}
