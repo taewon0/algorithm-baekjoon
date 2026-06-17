919cfd056836f8a543d7a91ff98bf32a3dacaa6f
taewon kim <128652249+taewon0@users.noreply.github.com>
2026-06-17 10:28:28 +0900

    [level 2] Title: 땅따먹기, Time: 15.15 ms, Memory: 91.3 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,14 @@
+class Solution {
+    int solution(int[][] land) {
+
+        for (int i = 1; i < land.length; i++) {
+            land[i][0] += Math.max(land[i-1][1], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][1] += Math.max(land[i-1][0], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][2] += Math.max(land[i-1][1], Math.max(land[i-1][0], land[i-1][3]));
+            land[i][3] += Math.max(land[i-1][1], Math.max(land[i-1][2], land[i-1][0]));
+        }
+
+        int l = land.length - 1;
+        return Math.max(land[l][0], Math.max(land[l][1], Math.max(land[l][2], land[l][3])));
+    }
+}
